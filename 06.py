609c5f13f31191c849d53e2b90e5de98689b0665
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-28 21:23:39 -0300

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,6 @@
+x1 = float(input('digite a coordenada de x1:  '))
+y1 = float(input('digite a coordenada de y1:  '))
+x2 = float(input('digite a coordenada de x2:  '))
+y2 = float(input('digite a coordenada de y2:  '))
+dist = ((x1-x2)**2 + (y1-y2)**2) **0.5
+print ('a distancia entre as coodenadas é de {dist} ') 

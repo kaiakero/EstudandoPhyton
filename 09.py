295dd1ed66bb8a857ad25d3c3193780d5d5abd71
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-29 22:15:22 -0300

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,4 @@
+dia = int(input('informe a quantidade de dias em que o carro foi alugado:  '))
+km = float(input('informe a quantidade de quilometros rodados com o carro:  '))
+preco = dia*60 + km*0.15
+print (f'o preço total a ser pago é de {preco}')

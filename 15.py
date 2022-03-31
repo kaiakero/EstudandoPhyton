22d534f67c955aea6ce1b8de83aef4d890c607ce
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-30 21:34:39 -0300

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,9 @@
+n1 = float(input('Informe o valor de n1:  '))
+
+
+if n1>0:
+    print('O valor informado é positivo')
+elif n1==0:
+    print('o valor informado é igual a zero')
+else :
+    print('o valor informado é negativo ')

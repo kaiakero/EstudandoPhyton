9313a6570ba3b4840706609815157d69a8701c36
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-30 21:56:49 -0300

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,7 @@
+n1 = float(input('Informe a primeira nota:  '))
+n2 = float(input('Informe a segunda nota:  '))
+media = n1+n2
+if media >= 12:
+    print('voce foi aprovado')
+elif media < 12:
+    print('voce foi reprovado')

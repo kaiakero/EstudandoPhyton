0d112269c73977657ae0a57caed970c956bf4e76
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-30 21:29:19 -0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,8 @@
+n1 = float(input('informe o valor de N1:  '))
+n2 = float(input('informe o valor de N2:  '))
+if n1>n2:
+    print(f'o maior valor informado foi  {n1}')
+elif n2==n1:
+    print('os valores informados são iguais')
+else:
+    print(f'o maior valor informado foi  {n2}')

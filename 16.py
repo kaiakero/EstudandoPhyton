c922ba0c0555e642d75215c055e98647c0cccc15
kaiakero <98500066+kaiakero@users.noreply.github.com>
2022-03-30 21:43:55 -0300

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,11 @@
+s = input('informe  seu sexo, usando "F" para feminino ou "M" para masculino:  ')
+if s=='f':
+    print('o sexo informado foi Feminino')
+elif s=='F':
+    print('o sexo informado foi Feminino')
+elif s=='m':
+    print('o sexo informado foi Masculino')
+elif s=='M':
+    print('o sexo informado foi Masculino')
+else:
+    print('o sexo informado é invalido')

a9980e46d8063f616712420a9d128b2396da3c2e
Vitor Pinto <98500066+kaiakero@users.noreply.github.com>
2022-04-07 21:30:52 -0300

    Add files via upload

---
+++ 07.04.2.py
@@ -0,0 +1,9 @@
+v1 = float(input('informe o primeiro valor'))
+v2 = float(input('informe o segundo valor'))
+v3 = float(input('informe o terceiro valor'))
+if v1>=v2 and v2>=v3:
+           print(v1)
+elif v2>=v3 and v3>=v2:
+    print (v2)
+else:
+    print (v3)

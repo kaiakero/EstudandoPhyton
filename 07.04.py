318a99f0a8724ea1284cc579c1d95617a329d111
Vitor Pinto <98500066+kaiakero@users.noreply.github.com>
2022-04-07 21:31:27 -0300

    Add files via upload

---
+++ 07.04.py
@@ -0,0 +1,19 @@
+equipamento = []
+valor = []
+serie = []
+setor = []
+resposta = 'SIM'
+
+while resposta == 'SIM':
+    equipamento.append(input('Equipamento:  '))
+    valor.append(float(input('Valor estimado do Equipamento:  ')))
+    serie.append(int(input('Informe o numero de serie do equipamento:  ')))
+    setor.append(input('Informe o setor do equipameto:  '))
+    resposta = str(input('digite "sim" para adicionar um elemento')).upper()
+
+    for indice in range(0,len(equipamento)):
+        print ('Equipamento', (indice+1))
+        print (f'nome......:  {equipamento[indice]}')
+        print (f'valor......:  {valor[indice]}')
+        print (f'serial......:  {serie[indice]}')
+        print (f'setor......:  {setor[indice]}')
